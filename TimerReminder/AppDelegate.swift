5fc320c1c0d6c97e5f9c0fc331e66384608669a3
Sweeper777 <sumulang@gmail.com>
2016-07-25 09:16:57 +0800

    prettified some things

--- TimerReminder/AppDelegate.swift
+++ TimerReminder/AppDelegate.swift
@@ -1,13 +1,6 @@
-//
-//  AppDelegate.swift
-//  TimerReminder
-//
-//  Created by Mulang Su on 7/24/16.
-//  Copyright © 2016 Mulang Su. All rights reserved.
-//
-
 import UIKit
 import CoreData
+import EZSwiftExtensions
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        window?.tintColor = UIColor(hexString: "5abb5a")
         return true
     }
 
