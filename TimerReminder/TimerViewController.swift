86919c5f622542bcf8fb189a4cf7c807efcc0a45
Sweeper777 <sumulang@gmail.com>
2016-07-25 09:23:02 +0800

    added bar button items

--- TimerReminder/TimerViewController.swift
+++ TimerReminder/TimerViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 import LTMorphingLabel
 import FittableFontLabel
 import EZSwiftExtensions
+import RWDropdownMenu
 
 class TimerViewController: UIViewController {
     @IBOutlet var timerLabel: LTMorphingLabel!
@@ -12,5 +13,21 @@ class TimerViewController: UIViewController {
         timerLabel.morphingEffect = .Fall
         timerLabel.fontSizeToFit()
         timerLabel.morphingEnabled = true
+    }
+    
+    @IBAction func more(sender: AnyObject) {
+        let menuItems = [
+            RWDropdownMenuItem(text: NSLocalizedString("Choose Timer", comment: ""), image: UIImage(named: "choose")) {
+                
+            },
+            RWDropdownMenuItem(text: NSLocalizedString("Add New Timer", comment: ""), image: UIImage(named: "add")) {
+                
+            },
+            RWDropdownMenuItem(text: NSLocalizedString("Set Timer", comment: ""), image: UIImage(named: "timer")) {
+                
+            }
+        ]
+        
+        RWDropdownMenu.presentFromViewController(self, withItems: menuItems, align: .Right, style: .Translucent, navBarImage: nil, completion: nil)
     }
 }
