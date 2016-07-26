b5be1fbd82e5b976c1eb3f9f01d1f8bb6b349ef2
Sweeper777 <sumulang@gmail.com>
2016-07-26 11:10:18 +0800

    generated NSManagedObject subclasses

---
+++ Reminder+CoreDataProperties.swift
@@ -0,0 +1,10 @@
+import Foundation
+import CoreData
+
+extension Reminder {
+
+    @NSManaged var remindTimeFrame: NSNumber?
+    @NSManaged var customRemindMessage: String?
+    @NSManaged var timer: TimerEntity?
+
+}
---
+++ TimerEntity+CoreDataProperties.swift
@@ -0,0 +1,14 @@
+import Foundation
+import CoreData
+
+extension TimerEntity {
+
+    @NSManaged var name: String?
+    @NSManaged var beepSounds: NSNumber?
+    @NSManaged var countDownTime: NSNumber?
+    @NSManaged var timesUpMessage: String?
+    @NSManaged var regularReminderInterval: NSNumber?
+    @NSManaged var timesUpSound: String?
+    @NSManaged var reminders: NSOrderedSet?
+
+}
