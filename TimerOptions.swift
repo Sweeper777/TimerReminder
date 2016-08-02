73e5593e544e4724d1603c43144e7b2841b69e74
Sweeper777 <sumulang@gmail.com>
2016-08-02 11:43:21 +0800

    fixed a compiler error

--- TimerOptions.swift
+++ TimerOptions.swift
@@ -1,15 +1,17 @@
 import Foundation
 import CoreData
-
+import UIKit
 
 class TimerOptions: NSManagedObject {
     static let defaultOptions: TimerOptions = {
-        let options = TimerOptions()
+        let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        let options = TimerOptions.init(entity: NSEntityDescription.entityForName("TimerOptions", inManagedObjectContext: context)!, insertIntoManagedObjectContext: nil)
+        options.initializeWithDefValues()
         options.name = "Default"
         return options
     }()
     
-    func initializeWithDefvalues() {
+    func initializeWithDefValues() {
         self.name = "Unnamed"
         self.beepSounds = false
         self.countDownTime = 10
