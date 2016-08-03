82d8273588f404796eef2a7ce233cfafee2fd40b
Sweeper777 <sumulang@gmail.com>
2016-08-03 11:55:31 +0800

    added options to switch between stopwatch and timer mode

--- TimerReminder/TimerViewController.swift
+++ TimerReminder/TimerViewController.swift
@@ -31,12 +31,9 @@ class TimerViewController: UIViewController, LTMorphingLabelDelegate {
             shortFontSize = timerLabel.fontSizeThatFits(text: "00:00", maxFontSize: 500)
             longFontSize = timerLabel.fontSizeThatFits(text: "00:00:00", maxFontSize: 500)
             timerLabel.font = timerLabel.font.fontWithSize(shortFontSize)
-//            timer = CountDownTimer(time: 60, onTimerChange: {
-//                self.timerLabel.text = $0.description
-//                }, onEnd: nil)
-            timer = CountUpTimer {
+            timer = CountDownTimer(time: 60, onTimerChange: {
                 self.timerLabel.text = $0.description
-            }
+                }, onEnd: nil)
             initializedFontSizes = true
         }
     }
@@ -61,17 +58,38 @@ class TimerViewController: UIViewController, LTMorphingLabelDelegate {
     }
     
     @IBAction func more(sender: AnyObject) {
-        let menuItems = [
+        var menuItems = [
             RWDropdownMenuItem(text: NSLocalizedString("Choose Timer", comment: ""), image: UIImage(named: "choose")) {
                 
             },
             RWDropdownMenuItem(text: NSLocalizedString("Add New Timer", comment: ""), image: UIImage(named: "add")) {
                 
-            },
-            RWDropdownMenuItem(text: NSLocalizedString("Set Timer", comment: ""), image: UIImage(named: "timer")) {
-                self.performSegueWithIdentifier("showSetTimer", sender: self)
             }
         ]
+        
+        if timer.canBeSet {
+            menuItems.append(RWDropdownMenuItem(text: NSLocalizedString("Set Timer", comment: ""), image: UIImage(named: "timer")) {
+                self.performSegueWithIdentifier("showSetTimer", sender: self)
+                })
+        }
+        
+        if timer is CountDownTimer {
+            menuItems.append(RWDropdownMenuItem(text: NSLocalizedString("Switch to Stopwatch Mode", comment: ""), image: UIImage(named: "countup")) {
+                self.timer = CountUpTimer {
+                    self.timer.reset()
+                    self.playButton.image = UIImage(named: "play")
+                    self.timerLabel.text = $0.description
+                }
+            })
+        } else if timer is CountUpTimer {
+            menuItems.append(RWDropdownMenuItem(text: NSLocalizedString("Switch to Timer Mode", comment: ""), image: UIImage(named: "countdown")) {
+                self.timer.reset()
+                self.playButton.image = UIImage(named: "play")
+                self.timer = CountDownTimer(time: 60, onTimerChange: {
+                    self.timerLabel.text = $0.description
+                    }, onEnd: nil)
+                })
+        }
         
         RWDropdownMenu.presentFromViewController(self, withItems: menuItems, align: .Right, style: .Translucent, navBarImage: nil, completion: nil)
     }
