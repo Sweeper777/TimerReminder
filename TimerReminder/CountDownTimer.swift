0e34869a1544690706dc80946e197e8d57364e1a
Sweeper777 <sumulang@gmail.com>
2016-08-05 15:13:38 +0800

    added normalise method for speech synthesiser

--- TimerReminder/CountDownTimer.swift
+++ TimerReminder/CountDownTimer.swift
@@ -4,6 +4,7 @@ class CountDownTimer: Timer {
     var beepSoundPlayer: AVAudioPlayer?
     var timeLeft: NSTimeInterval
     let timeToMeasure: NSTimeInterval
+    let synthesizer = AVSpeechSynthesizer()
     var timer: NSTimer?
     var paused = true
     var onEnd: ((Timer) -> Void)?
@@ -94,6 +95,8 @@ class CountDownTimer: Timer {
         self.options = options
     }
     
+
+    
     init(time: NSTimeInterval, options: TimerOptions? = nil, onTimerChange: ((Timer) -> Void)?, onEnd: ((Timer) -> Void)?) {
         self.timeLeft = time
         self.timeToMeasure = time
@@ -116,5 +119,35 @@ extension String {
             return string + self
         }
         return self
+    }
+}
+
+extension NSTimeInterval {
+    func normalized() -> String {
+        let hours = Int(self) % 86400 / 60 / 60
+        let minutes = Int(self) % 3600 / 60
+        let seconds = Int(self) % 60
+        
+        var normalized = ""
+        
+        if hours == 1 {
+            normalized += "\(hours) \(NSLocalizedString("Hour", comment: "")) "
+        } else if hours != 0 {
+            normalized += "\(hours) \(NSLocalizedString("Hours", comment: "")) "
+        }
+        
+        if minutes == 1 {
+            normalized += "\(minutes) \(NSLocalizedString("Minute", comment: "")) "
+        } else if minutes != 0 {
+            normalized += "\(minutes) \(NSLocalizedString("Minutes", comment: "")) "
+        }
+        
+        if seconds == 1 {
+            normalized += "\(seconds) \(NSLocalizedString("Second", comment: "")) "
+        } else if seconds != 0 {
+            normalized += "\(seconds) \(NSLocalizedString("Seconds", comment: "")) "
+        }
+        
+        return normalized
     }
 }
