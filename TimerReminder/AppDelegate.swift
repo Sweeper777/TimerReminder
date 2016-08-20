e933d3d4169a38d6f57c89b3374109d0ef47fe46
Sweeper777 <sumulang@gmail.com>
2016-08-20 07:41:14 +0100

    started tracking build numbers

--- TimerReminder/AppDelegate.swift
+++ TimerReminder/AppDelegate.swift
@@ -11,6 +11,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window?.tintColor = UIColor(hexString: "3b7b3b")
         UINavigationBar.appearance().barStyle = .Black
+        
+        lastUsedBuild = Int(ez.appBuild ?? "0") ?? 0
         return true
     }
 
@@ -103,3 +105,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+var lastUsedBuild: Int {
+    get { return NSUserDefaults.standardUserDefaults().integerForKey("lastUsedBuild") }
+    set { NSUserDefaults.standardUserDefaults().setInteger(newValue, forKey: "lastUsedBuild") }
+}
