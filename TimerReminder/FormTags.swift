f242525ea0d09267633c0374fef5ba6fe61f8ef1
Sweeper777 <sumulang@gmail.com>
2016-08-29 08:13:03 +0100

    added new row for the timer animation setting

--- TimerReminder/FormTags.swift
+++ TimerReminder/FormTags.swift
@@ -17,3 +17,4 @@ let tagRemindMessage = "remindMessage"
 
 let tagGestureControls = "gestureControls"
 let tagFontStyle = "fontStyle"
+let tagAnimation = "animation"
--- TimerReminder/GlobalSettingsController.swift
+++ TimerReminder/GlobalSettingsController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Eureka
+import LTMorphingLabel
 
 class GlobalSettingsController: FormViewController {
     weak var delegate: GlobalSettingsControllerDelegate?
@@ -20,19 +21,29 @@ class GlobalSettingsController: FormViewController {
                 row in
                 row.title = NSLocalizedString("Gesture Controls", comment: "")
                 row.value = NSUserDefaults.standardUserDefaults().boolForKey("gestureControl")
-            }.onChange {
+        }.onChange {
                 row in
                 self.delegate?.globalSettings?(self, globalSettingsDidChangeWithKey: "gestureControl", newValue: row.value!)
-            }
+        }
         
-        +++ SegmentedRow<String>(tagFontStyle) {
-            row in
-            row.title = NSLocalizedString("Font Style", comment: "")
-            row.options = ["Light", "Regular"]
-            row.value = NSUserDefaults.standardUserDefaults().integerForKey("fontStyle") == 1 ? "Regular" : "Light"
+            +++ SegmentedRow<String>(tagFontStyle) {
+                row in
+                row.title = NSLocalizedString("Font Style", comment: "")
+                row.options = ["Light", "Regular"]
+                row.value = NSUserDefaults.standardUserDefaults().integerForKey("fontStyle") == 1 ? "Regular" : "Light"
         }.onChange {
             row in
             self.delegate?.globalSettings?(self, globalSettingsDidChangeWithKey: "fontStyle", newValue: row.value!)
+        }
+        
+            +++ PickerInlineRow<LTMorphingEffect>(tagAnimation) {
+                row in
+                row.title = NSLocalizedString("Timer Animation", comment: "")
+                row.value = LTMorphingEffect(rawValue: NSUserDefaults.standardUserDefaults().integerForKey("timerAnimation"))!
+                row.options = [.Evaporate, .Scale, .Pixelate, .Fall, .Burn]
+        }.onChange {
+            row in
+            self.delegate?.globalSettings?(self, globalSettingsDidChangeWithKey: "timerAnimation", newValue: row.value!.rawValue)
         }
     }
 }
