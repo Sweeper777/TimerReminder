8b749929b7e5a23a421326f0fd294c801b0308d9
Sweeper777 <sumulang@gmail.com>
2017-07-08 07:42:33 +0100

    changed Clock to use another font

--- TimerReminder/Clock.swift
+++ TimerReminder/Clock.swift
@@ -39,7 +39,7 @@ class Clock: Timer {
     init(options: TimerOptions? = nil, onTimerChange: ((Timer) -> Void)?) {
         self.options = options
         self.onTimerChange = onTimerChange
-        formatter.dateFormat = "HH:mm"
+        formatter.dateFormat = "HH˸mm"
         let date = Date()
         description = formatter.string(from: date)
         onTimerChange?(self)
