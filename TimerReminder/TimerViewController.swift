b2e7756d59703625891a725fa1026491f5c19cfa
Mulang Su <sumulang@gmail.com>
2020-04-20 09:29:46 +0100

    Create TimerViewController.swift

---
+++ TimerReminder/TimerViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import LTMorphingLabel
+import RxSwift
+
+class TimerViewController: UIViewController {
+    
+    @IBOutlet var timerLabel: LTMorphingLabel!
+    @IBOutlet var playButton: UIButton!
+    
+}
