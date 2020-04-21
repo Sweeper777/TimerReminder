2d6fb23725be1a34888fdca1f39715f2066a2796
Mulang Su <sumulang@gmail.com>
2020-04-21 14:40:59 +0100

    made timer label font size auto fit

--- TimerReminder/TimerViewController.swift
+++ TimerReminder/TimerViewController.swift
@@ -7,4 +7,17 @@ class TimerViewController: UIViewController {
     @IBOutlet var timerLabel: LTMorphingLabel!
     @IBOutlet var playButton: UIButton!
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        timerLabel.updateFontSizeToFit()
+    }
+    
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animateAlongsideTransition(in: nil, animation: nil) { (_) in
+            self.timerLabel.updateFontSizeToFit()
+        }
+    }
 }
