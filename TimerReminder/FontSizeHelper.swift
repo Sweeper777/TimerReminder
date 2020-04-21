84aff34fae040e772dc8d1d8033b6111d5dd561a
Mulang Su <sumulang@gmail.com>
2020-04-21 14:40:18 +0100

    Create FontSizeHelper.swift

---
+++ TimerReminder/FontSizeHelper.swift
@@ -0,0 +1,18 @@
+import LTMorphingLabel
+
+fileprivate func shouldContinueToEnlarge(targetSize: CGSize, currentSize: CGSize) -> Bool {
+    return targetSize.height > currentSize.height && targetSize.width > currentSize.width
+}
+
+func fontSizeThatFits(size: CGSize, text: NSString, font: UIFont) -> CGFloat {
+    var fontToTest = font.withSize(0)
+    var currentSize = text.size(withAttributes: [NSAttributedString.Key.font: fontToTest])
+    var fontSize = CGFloat(1)
+    while shouldContinueToEnlarge(targetSize: size, currentSize: currentSize) {
+        fontToTest = fontToTest.withSize(fontSize)
+        currentSize = text.size(withAttributes: [NSAttributedString.Key.font: fontToTest])
+        fontSize += 1
+    }
+    return fontSize - 1
+}
+
