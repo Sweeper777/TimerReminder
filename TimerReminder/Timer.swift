6c019b34e5abcb231dc1fb3a16a6c8109c09c9d8
Mulang Su <sumulang@gmail.com>
2020-04-21 15:40:45 +0100

    added Timer.init

--- TimerReminder/Timer.swift
+++ TimerReminder/Timer.swift
@@ -17,4 +17,14 @@ class Timer {
     private var currentState: Int
     private let resetState: Int
     
+    private let observable: Observable<Int>
+    
+    private init(options: TimerOptions, mode: TimerMode, currentState: Int, resetState: Int, observable: Observable<Int>) {
+        self.options = options
+        self.mode = mode
+        self.currentState = currentState
+        self.resetState = resetState
+        self.observable = observable
+    }
+    
 }
