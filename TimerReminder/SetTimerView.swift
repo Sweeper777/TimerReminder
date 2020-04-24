fddc2538872b0fa579545228ed800d2b896af024
Mulang Su <sumulang@gmail.com>
2020-04-24 21:11:22 +0100

    Create SetTimerView.swift

---
+++ TimerReminder/SetTimerView.swift
@@ -0,0 +1,11 @@
+import UIKit
+import SnapKit
+
+class SetTimerView: UIView {
+    var picker: UIPickerView!
+    var hourLabel: UILabel!
+    var minuteLabel: UILabel!
+    var secondLabel: UILabel!
+    var okButton: UIButton!
+    
+}
