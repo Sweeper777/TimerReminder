59f4d973823309dedad4eb3d6bbf27661ea3a11c
Mulang Su <sumulang@gmail.com>
2020-05-05 12:07:20 +0100

    added labels to snap kit constraints in set timer view

--- TimerReminder/SetTimerView.swift
+++ TimerReminder/SetTimerView.swift
@@ -32,10 +32,10 @@ class SetTimerView: UIView {
         okButton.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
         self.addSubview(okButton)
         okButton.snp.makeConstraints { (make) in
-            make.width.equalTo(22)
-            make.height.equalTo(22)
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().offset(-8)
+            make.width.equalTo(22).labeled("ok button width = 22")
+            make.height.equalTo(22).labeled("ok button height = 22")
+            make.centerX.equalToSuperview().labeled("ok button is centered horizontally")
+            make.bottom.equalToSuperview().offset(-8).labeled("ok button is positioned at the bottom")
         }
         okButton.addTarget(self, action: #selector(okButtonDidPress), for: .touchUpInside)
         
@@ -44,10 +44,10 @@ class SetTimerView: UIView {
         hourLabel.textAlignment = .center
         self.addSubview(hourLabel)
         hourLabel.snp.makeConstraints { (make) in
-            make.bottom.equalTo(okButton.snp.top).offset(-8)
-            make.left.equalToSuperview()
-            make.width.equalToSuperview().dividedBy(3)
-            make.height.equalTo(20)
+            make.bottom.equalTo(okButton.snp.top).offset(-8).labeled("hour label is on top of ok button")
+            make.left.equalToSuperview().labeled("hour label is on the leftmost of set timer view")
+            make.width.equalToSuperview().dividedBy(3).labeled("hour label width is a third of superview's width")
+            make.height.equalTo(20).labeled("hour label height = 20")
         }
         hourLabel.translatesAutoresizingMaskIntoConstraints = true
         
@@ -56,10 +56,10 @@ class SetTimerView: UIView {
         minuteLabel.textAlignment = .center
         self.addSubview(minuteLabel)
         minuteLabel.snp.makeConstraints { (make) in
-            make.bottom.equalTo(okButton.snp.top).offset(-8)
-            make.left.equalTo(hourLabel.snp.right)
-            make.width.equalToSuperview().dividedBy(3)
-            make.height.equalTo(20)
+            make.bottom.equalTo(okButton.snp.top).offset(-8).labeled("minute label is on top of ok button")
+            make.left.equalTo(hourLabel.snp.right).labeled("minute label is on the right of hour label")
+            make.width.equalToSuperview().dividedBy(3).labeled("minute label width is a third of super view's width")
+            make.height.equalTo(20).labeled("minute label height = 20")
         }
         
         secondLabel = UILabel()
@@ -67,10 +67,10 @@ class SetTimerView: UIView {
         secondLabel.textAlignment = .center
         self.addSubview(secondLabel)
         secondLabel.snp.makeConstraints { (make) in
-            make.bottom.equalTo(okButton.snp.top).offset(-8)
-            make.left.equalTo(minuteLabel.snp.right)
-            make.width.equalToSuperview().dividedBy(3)
-            make.height.equalTo(20)
+            make.bottom.equalTo(okButton.snp.top).offset(-8).labeled("second label is on top of ok button")
+            make.left.equalTo(minuteLabel.snp.right).labeled("second label is on the right of minute label")
+            make.width.equalToSuperview().dividedBy(3).labeled("second label width is a third of super view's width")
+            make.height.equalTo(20).labeled("second label height = 20")
         }
         
         picker = UIPickerView()
@@ -78,10 +78,10 @@ class SetTimerView: UIView {
         picker.delegate = self
         self.addSubview(picker)
         picker.snp.makeConstraints { (make) in
-            make.bottom.equalTo(secondLabel.snp.top).offset(-8)
-            make.left.equalToSuperview()
-            make.right.equalToSuperview()
-            make.top.equalToSuperview()
+            make.bottom.equalTo(secondLabel.snp.top).offset(-8).labeled("picker is on top of second label")
+            make.left.equalToSuperview().labeled("picker sticks to the left side")
+            make.right.equalToSuperview().labeled("oicker sticks to the right side")
+            make.top.equalToSuperview().labeled("picker sticks to the top side")
         }
         
         subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
