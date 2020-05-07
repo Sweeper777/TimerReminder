ea57d2952d6021b3e54fe1b5ad4969d4ceb39a40
Mulang Su <sumulang@gmail.com>
2020-05-07 10:02:17 +0100

    added TimerOption realm object

--- TimerReminder/TimerOptionsObject.swift
+++ TimerReminder/TimerOptionsObject.swift
@@ -5,3 +5,18 @@ class ReminderObject : Object {
     @objc dynamic var message: String? = nil
 }
 
+class TimerOptionsObject : Object {
+    @objc dynamic var name = "Default"
+    @objc dynamic var language = "en"
+    let countDownTime = RealmOptional<Int>(10)
+    @objc dynamic var countSeconds = false
+    @objc dynamic var beepSounds = false
+    @objc dynamic var vibrate = false
+    @objc dynamic var font = 1
+    @objc dynamic var textAnimation = 0
+    @objc dynamic var timeUpMessage: String? = nil
+    @objc dynamic var timeUpSound: String? = nil
+    @objc dynamic var regularReminders = false
+    let reminders = List<ReminderObject>()
+    
+}
