3394d5e19335d60ccf5696c721d4eea7e2db8272
Mulang Su <sumulang@gmail.com>
2020-05-07 10:02:48 +0100

    added initialisers to convert to realm objects

--- TimerReminder/TimerOptionsObject.swift
+++ TimerReminder/TimerOptionsObject.swift
@@ -3,6 +3,16 @@ import RealmSwift
 class ReminderObject : Object {
     @objc dynamic var remindTime = 0
     @objc dynamic var message: String? = nil
+    
+    init(reminder: Reminder) {
+        super.init()
+        remindTime = reminder.remindTime
+        message = reminder.message
+    }
+    
+    required init() {
+        super.init()
+    }
 }
 
 class TimerOptionsObject : Object {
@@ -19,4 +29,43 @@ class TimerOptionsObject : Object {
     @objc dynamic var regularReminders = false
     let reminders = List<ReminderObject>()
     
+    init(timerOptions: TimerOptions) {
+        super.init()
+        name = timerOptions.name
+        language = timerOptions.language
+        switch timerOptions.countDown {
+        case .no:
+            countDownTime.value = nil
+        case .yes(startsAt: let x):
+            countDownTime.value = x
+        }
+        countSeconds = timerOptions.countSeconds
+        beepSounds = timerOptions.beepSounds
+        vibrate = timerOptions.vibrate
+        font = timerOptions.font.rawValue
+        textAnimation = timerOptions.textAnimation.rawValue
+        switch timerOptions.timeUpOption {
+        case .playSound(let s):
+            timeUpSound = s
+        case .speak(let s):
+            timeUpMessage = s
+        case .speakDefaultMessage:
+            break
+        }
+        
+        switch timerOptions.reminderOption {
+        case .regularInterval(let reminder):
+            regularReminders = true
+            reminders.append(ReminderObject(reminder: reminder))
+        case .specificTimes(let reminders):
+            regularReminders = false
+            self.reminders.append(objectsIn: reminders.map(ReminderObject.init))
+        case .no:
+            break
+        }
+    }
+    
+    required init() {
+        super.init()
+    }
 }
