e28f1d020ccdbb64404d2a23d58b15dbd76905f5
Mulang Su <sumulang@gmail.com>
2020-05-13 21:18:09 +0100

    Create CurrentOptionsViewController.swift

---
+++ TimerReminder/CurrentOptionsViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import Eureka
+import AVFoundation
+
+class CurrentOptionsViewController: FormViewController {
+    var tableViewTopInset: CGFloat?
+    var showNameField = false
+    var player: AVAudioPlayer?
+    
+}
+
