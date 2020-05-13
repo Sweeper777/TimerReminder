4069b24de8ca1f67c3971e1030954ee9d556cdde
Mulang Su <sumulang@gmail.com>
2020-05-13 21:17:21 +0100

    set up tab page VC

--- TimerReminder/MySlideViewController.swift
+++ TimerReminder/MySlideViewController.swift
@@ -4,6 +4,13 @@ import TabPageViewController
 class MySlideViewController: SlideMenuController {
 
     override func awakeFromNib() {
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "main") {
+            self.mainViewController = controller
+        }
+        let tabPageViewController = TabPageViewController.create()
+        tabPageViewController.option.currentColor = UIColor(hex: "5abb5a")
+        tabPageViewController.option.tabBackgroundColor = .systemBackground
+        tabPageViewController.option.pageBackgoundColor = .systemBackground
         super.awakeFromNib()
     }
 
