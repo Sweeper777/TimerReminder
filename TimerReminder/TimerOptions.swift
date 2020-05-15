fc833baaf5ec19b21930292788d726f9df32ed12
Mulang Su <sumulang@gmail.com>
2020-05-15 21:00:31 +0100

    added CountDownTime enum

--- TimerReminder/TimerOptions.swift
+++ TimerReminder/TimerOptions.swift
@@ -121,3 +121,18 @@ extension TimerOptions {
         )
     }
 }
+
+enum CountDownTime: Int, CustomStringConvertible {
+    
+    var description: String {
+        return "\(rawValue) \("Seconds".localised)"
+    }
+    
+    case _3 = 3
+    case _5 = 5
+    case _10 = 10
+    case _20 = 20
+    case _30 = 30
+    case _60 = 60
+}
+
