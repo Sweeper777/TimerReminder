18960ea2e587b5177dbc4e7c98fea974680fd316
Mulang Su <sumulang@gmail.com>
2020-05-22 08:37:16 +0100

    used a custom cell for TimeIntervalRow

--- TimerReminder/TimeIntervalRow.swift
+++ TimerReminder/TimeIntervalRow.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Eureka
 
-final class TimeIntervalRow: SelectorRow<AlertSelectorCell<Int>>, RowType {
+final class TimeIntervalRow: SelectorRow<TimeIntervalCell>, RowType {
     public required init(tag: String?) {
         super.init(tag: tag)
         presentationMode = .presentModally(controllerProvider: .storyBoard(storyboardId: "timeIntervalPicker", storyboardName: "Main", bundle: Bundle.main),
@@ -19,5 +19,28 @@ final class TimeIntervalRow: SelectorRow<AlertSelectorCell<Int>>, RowType {
             formatter.zeroFormattingBehavior = .pad
             return formatter.string(from: TimeInterval(timeInterval))
         }
+    }
+    
+    override func customDidSelect() {
+        super.customDidSelect()
+        deselect(animated: true)
+        updateCell()
+    }
+}
+
+class TimeIntervalCell: AlertSelectorCell<Int> {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        contentView.backgroundColor = .systemGray3
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        contentView.backgroundColor = .secondarySystemGroupedBackground
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        contentView.backgroundColor = .secondarySystemGroupedBackground
     }
 }
