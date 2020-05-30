1008459da03dab152abd95f62aebde09fcade608
Mulang Su <sumulang@gmail.com>
2020-05-30 12:32:30 +0100

    added done button in MySlideVC

--- TimerReminder/MySlideViewController.swift
+++ TimerReminder/MySlideViewController.swift
@@ -1,5 +1,6 @@
 import SlideMenuControllerSwift
 import TabPageViewController
+import MaterialComponents
 
 class MySlideViewController: SlideMenuController {
 
@@ -24,6 +25,21 @@ class MySlideViewController: SlideMenuController {
         if traitCollection.horizontalSizeClass == .regular && traitCollection.verticalSizeClass == .regular {
             SlideMenuOptions.rightViewWidth *= 2
         }
+        
+        let doneButton = MDCFloatingButton(shape: .default)
+        doneButton.setImage(UIImage(systemName: "checkmark"), for: .normal)
+        doneButton.imageView?.tintColor = .white
+        doneButton.backgroundColor = UIColor(named: "tint")
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        tabPageViewController.view.addSubview(doneButton)
+        doneButton.snp.makeConstraints { (make) in
+            make.right.equalToSuperview().offset(-20).labeled("done button on the rightmost of screen")
+        }
+        let doneBottomConstraint = doneButton.bottomAnchor.constraint(equalTo: tabPageViewController.view.keyboardLayoutGuide.topAnchor)
+        doneBottomConstraint.constant = -40
+        doneBottomConstraint.isActive = true
+        doneButton.addTarget(self, action: #selector(doneTapped), for: .touchUpInside)
+        
         super.awakeFromNib()
     }
 
