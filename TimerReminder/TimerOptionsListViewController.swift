503fc66e398f45699e4b1f27c67689c0a03d7ad2
Mulang Su <sumulang@gmail.com>
2020-05-31 09:15:17 +0100

    implemented table view data source in options list VC

--- TimerReminder/TimerOptionsListViewController.swift
+++ TimerReminder/TimerOptionsListViewController.swift
@@ -6,6 +6,8 @@ import EmptyDataSet_Swift
 class TimerOptionsListViewController : UITableViewController {
     var tableViewTopInset: CGFloat?
     
+    var allTimerOptions: [TimerOptions] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -14,9 +16,25 @@ class TimerOptionsListViewController : UITableViewController {
             tableView.contentInset.bottom = 84
         }
         
-        tableView.emptyDataSetView { (view) in
-            view.titleLabelString(NSAttributedString(string: "You don't have any saved timer options!".localised))
-            view.detailLabelString(NSAttributedString(string: "Tap the '+' button to create one!".localised))
+        allTimerOptions = TimerOptionsManager.shared.allTimerOptions
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTimerOptions.count + 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "addButton")!
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+            cell.textLabel?.text = allTimerOptions[indexPath.row + 1].name
+            return cell
         }
     }
 }
