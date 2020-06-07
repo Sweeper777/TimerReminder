67aa22b7be4708cc057ab4d3331e88539065ff7d
Mulang Su <sumulang@gmail.com>
2020-06-07 10:14:00 +0100

    tried to set up the things necessary for core data migration

--- TimerReminder/AppDelegate.swift
+++ TimerReminder/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Firebase
+import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -38,7 +40,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
 
     }
+    
+    lazy var applicationDocumentsDirectory: URL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.TimerReminder" in the application's documents Application Support directory.
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls[urls.count-1]
+    }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+         let modelURL = Bundle.main.url(forResource: "TimerReminder", withExtension: "momd")!
+         return NSManagedObjectModel(contentsOf: modelURL)!
+     }()
 
+     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+         // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+         // Create the coordinator and store
+         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+         
+         return coordinator
+     }()
+
+     lazy var managedObjectContext: NSManagedObjectContext = {
+         // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+         let coordinator = self.persistentStoreCoordinator
+         var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+         managedObjectContext.persistentStoreCoordinator = coordinator
+         return managedObjectContext
+     }()
 }
 
 var lastUsedBuild: Int {
