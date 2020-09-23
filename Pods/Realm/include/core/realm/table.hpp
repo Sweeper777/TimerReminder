89506b211b1e12ab8034b334635bd68eb1da6248
Mulang Su <sumulang@gmail.com>
2020-09-23 09:33:19 +0800

    updated all pods

--- Pods/Realm/include/core/realm/table.hpp
+++ Pods/Realm/include/core/realm/table.hpp
@@ -700,7 +700,8 @@ private:
 
     // Migration support
     void migrate_column_info();
-    void migrate_indexes(ColKey pk_col_key);
+    bool verify_column_keys();
+    void migrate_indexes();
     void migrate_subspec();
     void create_columns();
     bool migrate_objects(ColKey pk_col_key); // Returns true if there are no links to migrate
